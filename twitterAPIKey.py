2571d9d01d3a77d8ae5efb6550bdd1ffc9cf6f67
AIRICLEE <49914236+AIRICLEE@users.noreply.github.com>
2020-03-08 18:28:03 -0400

    Add twitterAPIKey.py

---
+++ twitterAPIKey.py
@@ -0,0 +1,9 @@
+# Enter your API keys here
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+
+# Enter what you want search in twitter
+# Here we search iphone 11 as example
+query = "iphone 11"
